ccf811dd01fc40ce00aa3f3a57afdc6cc702e220
brunosantos <bruno.luiz88@gmail.com>
2021-04-19 17:56:35 -0300

    incluido test para usecase

---
+++ domain/src/test/java/com/easynvest/domain/usecase/FetchInvestmentUseCaseTest.kt
@@ -0,0 +1,27 @@
+package com.easynvest.domain.usecase
+
+import com.easynvest.domain.model.InvestmentRequest
+import com.easynvest.domain.model.InvestmentResponse
+import com.easynvest.domain.repository.InvestmentRepository
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Test
+
+
+class FetchInvestmentUseCaseTest {
+
+    @Test
+    fun when_execute_should_call_fetchInvestmentData() = runBlockingTest {
+        val repository = mockk<InvestmentRepository>()
+        val response = mockk<InvestmentResponse>()
+        coEvery { repository.fetchInvestmentData(any()) } returns response
+
+        val request = mockk<InvestmentRequest>()
+        val useCase = FetchInvestmentUseCase(repository)
+        useCase.execute(request)
+
+        coVerify(exactly = 1) { repository.fetchInvestmentData(any()) }
+    }
+}
